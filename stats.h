a699e3f4d5326f24408a003cbbc1071669714b2d
Naman Chauhan <chauhannaman98@gmail.com>
2019-07-31 00:14:28 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,14 +12,21 @@
  * in arguements and Prints all of them in a fairly presentable way
  * in the output window.
  * 
- * @param min an in
+ * @param min, max, mean and median are integers
+ * @return void
  */
 void print_statistics(int min, int max, int mean, int median);
 
+
 /**
+ * @breif Prints all the array elements
  * 
+ * This funtion takes array and it's size to print all the elements
+ * of array 
  */
 void print_array(int array, int size);
+
+
 int median(int array, int size);
 int mean(int array, int size);
 int max(int array, int size);
