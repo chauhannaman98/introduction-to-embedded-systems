eb0a6078d04a0288ca817a87c8c7c1efcd1d084f
Naman Chauhan <chauhannaman98@gmail.com>
2019-08-03 01:02:43 +0530

    Update stats.h comments

--- stats.h
+++ stats.h
@@ -1,5 +1,6 @@
-/**The stats.h header file include all declarations and documentation for the functions from the
- *stats.c file. It have function declarations for all required functions except main.
+/**
+ * The stats.h header file include all declarations and documentation for the functions from the
+ * stats.c file. It have function declarations for all required functions except main.
  */
 
 #ifndef __STATS_H__
@@ -22,16 +23,66 @@ void print_statistics(int min, int max, int mean, int median);
  * @breif Prints all the array elements
  * 
  * This funtion takes array and it's size to print all the elements
- * of array 
+ * of array in the output window.
+ * 
+ * @param array and size are integers
+ * @return void
  */
 void print_array(int array, int size);
 
-
+/**
+ * @breif Returns the median of a sorted array
+ * 
+ * This function takes a sorted array in ascending order and 
+ * it's size to find and return the median of the array.
+ * 
+ * @param array and size are integers
+ * @return med is an integer
+ */
 int median(int array, int size);
-int mean(int array, int size);
-int max(int array, int size);
-int min(int array, int size);
-void sort(int array, int size);
 
+/**
+ * @breif Returns the mean of the array
+ * 
+ * This function takes an array and it's size and returns 
+ * the mean of the array elements.
+ * 
+ * @param array and size are integers
+ * @return avg is an integer
+ */
+int mean(int array, int size);
+
+/**
+ * @breif Returns the largest element of the array
+ * 
+ * This function takes an array and it's size and returns 
+ * the largest of the array elements.
+ * 
+ * @param array and size are integers
+ * @return lar is an integer
+ */
+int max(int array, int size);
+
+/**
+ * @breif Returns the smallest element of the array
+ * 
+ * This function takes an array and it's size and returns 
+ * the smallest of the array elements.
+ * 
+ * @param array and size are integers
+ * @return small is an integer
+ */
+int min(int array, int size);
+
+/**
+ * @breif Sorts the array elements in ascending order
+ * 
+ * This funtion takes array and it's size to organize elements
+ * of the array in ascending order.
+ * 
+ * @param array and size are integers
+ * @return void
+ */
+void sort(int array, int size);
 
 #endif
